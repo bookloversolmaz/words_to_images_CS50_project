a163ddf8d0c125fadba2fadaebb367e280dfc8a4
bookloverbabe <solmaz@hotmail.co.uk>
2023-07-26 18:01:29 +0100

    Created method for iterating and assigning values to inputs

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # This pages is where any configuration, registration, and other setup the application needs will happen. 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 # Creates a flask application object in the current python module
 app = Flask(__name__)
 
@@ -7,17 +7,40 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-@app.route("/images", methods=["GET", "POST"])
-def images():
-    if request.method == "POST":
-    # Code here assigns values to users input and outputs an image depending on those values
-    # a, e, i, o, u = 2 points
-    # b, j, k, q, v, x, y, z = 4 points
-    # c, d, f, g, h, l, m, n, p, r, s, t, w = 1 point
-    # <= 5: cat image, 6 to 15: dog image, 16 to 25: third image, 26 to 35: fourth image, 36+ fifth image
-        return render_template("images.html")
+# @app.route("/images", methods=["GET", "POST"])
+# def images():
+#     if request.method == "POST":
+#         return render_template("images.html", text=text)
+#     else:
+#         return render_template("home.html")
+
+def get_letter_value(letter):
+    vowel = 'aeiou'
+    consonant = 'bjkqvxz'
+    remainder = 'cdfghlmnprstw'
+    
+    if letter.lower() in vowel:
+        return 2
+    elif letter.lower() in consonant:
+        return 4
+    elif letter.lower() in remainder:
+        return 1
     else:
-        return render_template("home.html")
+        flash("Please enter text only")
+
+@app.route('/', methods=['GET', 'POST'])
+def calculate_letter_sum():
+    if request.method == 'POST':
+        text = request.form.get('text')
+        total_sum = 0
+
+        for letter in text:
+            total_sum += get_letter_value(letter)
+
+        total_sum
+    
+    return render_template('index.html')
+    # <= 5: cat image, 6 to 15: dog image, 16 to 25: third image, 26 to 35: fourth image, 36+ fifth image
 
 
 if __name__ == "__main__":
--- test_app.py
+++ test_app.py
@@ -1,5 +1,5 @@
 import unittest
-from app import app
+from app import app, get_letter_value
 
 # In Flask, the app object should not be called as a function (i.e., app()), 
 # but it should be used directly as the test client.
@@ -16,12 +16,25 @@ class TestApp(unittest.TestCase):
         # as response data is usually in bytes.
         self.assertIn(b'<h1>Words to images</h1>', response.data)
         self.assertIn (b'Enter a word or phrase here', response.data)
-    def test_images(self):
-        response = self.app.post("/images")
-        self.assertIn(b'<h1>Images</h1>', response.data)
-        # Test that there is an image tag
-        
-        response = self.app.get("/")
-        self.assertIn(b'<h1>Words to images</h1>', response.data)
+
+    # def test_images(self):
+    #     response = self.app.post("/images")
+    #     self.assertIn(b'<h1>Images</h1>', response.data)
+        # Checks user input
+        # response = self.app.get("/")
+        # self.assertIn(b'<h1>Words to images</h1>', response.data)
+
+    def test_get_letter_value(self):
+        response = get_letter_value('E')
+        self.assertEqual(response, 2)
+        response = get_letter_value('c')
+        self.assertEqual(response, 1)
+        response = get_letter_value('b')
+        self.assertEqual(response, 4)
+        response = get_letter_value(' ')
+        self.assertEqual(response, "Please enter text only")
+        response = get_letter_value('3')
+        self.assertEqual(response, "Please enter text only")
+
 if __name__ == "__main__":
     unittest.main()
